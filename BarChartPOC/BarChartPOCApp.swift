3811108f44f62519c255636df9fc1f323e94a569
Yosi <yosimizrachi7@gmail.com>
2021-02-03 17:26:25 +0200

    Initial Commit

---
+++ BarChartPOC/BarChartPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarChartPOCApp.swift
+//  BarChartPOC
+//
+//  Created by Yosi Mizrachi on 03/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct BarChartPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BarChartPOC/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BarChartPOC
+//
+//  Created by Yosi Mizrachi on 03/02/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
